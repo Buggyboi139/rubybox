d04b80593a98c3afc1bd753c853eb9b5413d13d7
Buggyboi139 <willworkforbugs@gmail.com>
2026-04-15 10:09:31 -0500

    Create toasts.js

---
+++ js/ui/toasts.js
@@ -0,0 +1,49 @@
+window.AppToasts = {
+    container: null,
+
+    init() {
+        if (!this.container) {
+            this.container = document.getElementById('toast-container');
+        }
+        if (!this.container) {
+            this.container = document.createElement('div');
+            this.container.id = 'toast-container');
+            document.body.appendChild(this.container);
+        }
+    },
+
+    show(message, type = 'info') {
+        this.init();
+
+        const toast = document.createElement('div');
+        toast.className = `toast ${type}`;
+        toast.textContent = message;
+
+        this.container.appendChild(toast);
+
+        requestAnimationFrame(() => {
+            toast.classList.add('show');
+        });
+
+        setTimeout(() => {
+            toast.classList.remove('show');
+            setTimeout(() => toast.remove(), 300);
+        }, 3000);
+    },
+
+    success(message) {
+        this.show(message, 'success');
+    },
+
+    error(message) {
+        this.show(message, 'error');
+    },
+
+    info(message) {
+        this.show(message, 'info');
+    },
+
+    warning(message) {
+        this.show(message, 'warning');
+    }
+};
