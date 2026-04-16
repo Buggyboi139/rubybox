0ccdc7a4569a7df4cc775322f8874b2b213987d6
Buggyboi139 <willworkforbugs@gmail.com>
2026-04-16 14:53:56 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'rubybox-v4';
+const CACHE_NAME = 'rubybox-v5';
 const assets = [
     '/',
     '/index.html',
@@ -43,6 +43,7 @@ const assets = [
 ];
 
 self.addEventListener('install', event => {
+    self.skipWaiting();
     event.waitUntil(
         caches.open(CACHE_NAME).then(cache => cache.addAll(assets))
     );
@@ -52,7 +53,7 @@ self.addEventListener('activate', event => {
     event.waitUntil(
         caches.keys().then(keys => Promise.all(
             keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
-        ))
+        )).then(() => self.clients.claim())
     );
 });
 
